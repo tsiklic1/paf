f71bb9bbbcf860c135aef9756fe492732d8258ca
tsiklic1 <tsiklic@pmfst.hr>
2022-03-08 21:38:43 +0100

    Vjezbe_1

--- Vjezbe_1/zadatak6.py
+++ Vjezbe_1/zadatak6.py
@@ -22,17 +22,26 @@ def polozaj(tocka, kruznica):
     
     udaljenost = abs(((tocka[0] - kruznica[0])**2 + (tocka[1] - kruznica[1])**2)**(1/2)-kruznica[2])
 
-    print("Udaljenost točke od kružnice je: ", udaljenost)
+    print("Udaljenost točke od kružnice je: ", udaljenost, ", a točka se nalazi na koordinatama (" + str(tocka[0]) + "," + str(tocka[1]) + ").")
 
     x = np.linspace(kruznica[0]-1*kruznica[2], kruznica[0]+kruznica[2], 100)
     y1 = (kruznica[2]**2 - (x-kruznica[0])**2)**(1/2) + kruznica[1]
     y2 = -(kruznica[2]**2 - (x-kruznica[0])**2)**(1/2) + kruznica[1]
 
-    plt.plot(x, y1, "c")
-    plt.plot(x, y2, "c")
-    plt.plot([kruznica[0]],[kruznica[1]], "bo")
-    plt.plot([tocka[0]],[tocka[1]], "ro")
+    fig = plt.figure(figsize=(4,4), dpi = 200)
+    axes = fig.add_axes([0.1, 0.1, 0.9, 0.9])
+    axes.set_aspect("equal")
+    axes.plot(x, y1, "c")
+    axes.plot(x, y2, "c")
+    axes.plot([kruznica[0]],[kruznica[1]], "bo")
+    axes.plot([tocka[0]],[tocka[1]], "ro")
     plt.show()
+
+    # plt.plot(x, y1, "c")
+    # plt.plot(x, y2, "c")
+    # plt.plot([kruznica[0]],[kruznica[1]], "bo")
+    # plt.plot([tocka[0]],[tocka[1]], "ro")
+    # plt.show()
 
 polozaj(tocka, kruznica)
     

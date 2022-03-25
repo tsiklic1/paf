de74c9b2e70ec94cc93fb6393d9d83c054c16326
tsiklic1 <tsiklic@pmfst.hr>
2022-03-25 20:58:15 +0100

    odkomentirana greska

--- Vjezbe_3/gibanje.py
+++ Vjezbe_3/gibanje.py
@@ -13,18 +13,18 @@ p1.ovisnostDometa(5)
 p1.ovisnostVremena(5)
 
 #Dio za graf ovisnosti greske o dt
-# listaDt = list(np.linspace(0.001,0.1,200))
-# listaDomet = []
-# analitickiDomet = 100 * np.sin(2*np.pi/3)/9.81
-# listaGresaka = []
-# for i in listaDt:
-#     p2=prt.Particle()
-#     p2.set_initial_conditions(10, np.pi/3, 0, 0, i )
-#     listaDomet.append(p2.range())
-#     listaGresaka.append(abs(listaDomet[-1] - analitickiDomet)*100/analitickiDomet)
+listaDt = list(np.linspace(0.001,0.1,200))
+listaDomet = []
+analitickiDomet = 100 * np.sin(2*np.pi/3)/9.81
+listaGresaka = []
+for i in listaDt:
+    p2=prt.Particle()
+    p2.set_initial_conditions(10, np.pi/3, 0, 0, i )
+    listaDomet.append(p2.range())
+    listaGresaka.append(abs(listaDomet[-1] - analitickiDomet)*100/analitickiDomet)
 
-# fig, axs = plt.subplots(1, figsize = (6,6))
-# axs.plot(listaDt, listaGresaka)
-# plt.setp(axs, xlabel = "dt (s)")
-# plt.setp(axs, ylabel = "pogreška (%)")
-# plt.show()
+fig, axs = plt.subplots(1, figsize = (6,6))
+axs.plot(listaDt, listaGresaka)
+plt.setp(axs, xlabel = "dt (s)")
+plt.setp(axs, ylabel = "pogreška (%)")
+plt.show()
